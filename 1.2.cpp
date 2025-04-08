f4824bac0d840e63022d3fb71ed90e028666827b
Hetvi-undhad <undhadhetvi235@gmail.com>
2025-04-08 17:48:33 +0530

    1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -1,3 +1,9 @@
+A small retail store is facing challenges in managing its inventory effectively. The store sells a ( )variety of products, each identified by a unique product ID,
+a name, the available quantity in ( )stock, and the price per unit. To streamline their operations, the store needs a basic system to ( )manage this inventory 
+efficiently. The system must provide the ability to add new products to the inventory, ensuring that each product has its ID, name, quantity, and price properly
+recorded. Additionally, the system should allow the store staff to update the quantity of any existing product, such as when new stock arrives or when items are
+sold. Another essential feature of the system is the calculation of the total value of all products in the inventory, which is determined by multiplying the 
+quantity of each product by its price and summing these values for all products.
 
 #include<iostream>
 using namespace std;
